d1082d57251fad5154f4f16590d38e22faf45aa8
yebiny <yebinyoun@gmail.com>
2021-01-20 15:10:58 +0900

    change name

--- myScript/TEST_/add_subj.sh
+++ myScript/TEST_/add_subj.sh
@@ -3,3 +3,7 @@
 sub_name=${1}
 mkdir exp_info/$sub_name
 mkdir exp_info/$sub_name/design
+mkdir results/$sub_name
+mkdir results/$sub_name/process
+mkdir results/$sub_name/1stLV
+mkdir results/$sub_name/2ndLV
