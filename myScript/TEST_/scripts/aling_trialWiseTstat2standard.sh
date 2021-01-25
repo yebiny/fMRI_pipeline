ecd3683b107a29a928bb6cf90c843f97bc8984ee
yebiny <yebinyoun@gmail.com>
2021-01-25 16:05:31 +0900

    add scripts

--- myScript/TEST_/scripts/aling_trialWiseTstat2standard.sh
+++ myScript/TEST_/scripts/aling_trialWiseTstat2standard.sh
@@ -2,8 +2,12 @@
 #aligning trial-wise tstat to standard space
 
 ## ######################## aligning training data points(100) to standard (note taht there was a glm problem for using training + test)
-source globals.sh
-STANDARD_BRAIN=/usr/local/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz
+source tool.sh
+
+SUBJ=sub01
+FIRSTLEVEL_DIR=results/$SUBJ/fsf
+MASK_DIR=results/$SUBJ/mask
+
 #################################### aligning tstat to standard
 for run in {1..10}; do
 	#training + test set
@@ -22,7 +26,7 @@ for run in {1..10}; do
 	for ii in {1..110}; do
 		inputfile=$FIRSTLEVEL_DIR/phase1_trialWiseGLM_train_run$run.feat/stats/tstat${ii}_standard.nii.gz
 		for mm in 1 2; do
-			mask=inter_output/gen_masks/mask${mm}.nii
+			mask=$MASK_DIR/mask${mm}.nii
 			outputfile=$FIRSTLEVEL_DIR/phase1_trialWiseGLM_train_run$run.feat/stats/tstat${ii}_mask${mm}.nii.gz
 			fslmaths $inputfile -mas $mask $outputfile
 		done
